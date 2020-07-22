97b9120da11cc18684aa35191a406dc6aaf647d4
Matias M <matiasmendoza1994@gmail.com>
2020-07-22 01:27:49 -0300

    Se agrega un ejemplo de la utilizacion

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,9 @@
 #! node
 
-// Challenge: Define and use a function in a new File
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the function printing the message to console
+const validator = require('validator')
 
-// const utils = require('./utils')
-// console.log(utils.add(5, 5))
+const emails = ['juan@pepe.com', 'pepe@pepa', 'sarasa@asda.c', 'asdads.com']
 
-const getNotes = require('./notes')
-
-console.log(getNotes())
+emails.forEach((email) =>{
+  console.log(`Is ${email} an email? ${validator.isEmail(email)}`);
+})
