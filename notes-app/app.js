532246fb209719e9f2687027c7d881839613f668
Matias M <matiasmendoza1994@gmail.com>
2020-08-06 01:58:55 -0300

    Se agregan los comandos para abm de una nota a traves de cli utilizando el parser de argumentos yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,4 @@
-#! node
+#! nodemon
 
 // Chalenge: Use chalk library in your app.js
 //
@@ -10,11 +10,87 @@
 // Bonus: Use does to mess around with other SVGElementInstanceList. Make text boldt and inversed
 
 const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes')
 
-const validator = require('validator')
+/* const validator = require('validator')
 
 const emails = ['juan@pepe.com', 'pepe@pepa', 'sarasa@asda.c', 'asdads.com']
 
-emails.forEach((email) =>{
+emails.forEach((email) => {
   console.log(`${chalk.bold(email)} ${validator.isEmail(email) ? chalk.green('is an email') : chalk.red('is not an email')}`)
-})}
+})
+ */
+
+/* console.log(process.argv)
+
+const command = process.argv[2]
+
+if (command === 'add') {
+  console.log('Adding note')
+} else if (command === 'remove') {
+  console.log('Removing note')
+}
+
+console.log(yargs.argv)
+
+ */
+// Customize yargs version
+
+yargs.version('1.1.1')
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => console.log(`adding the note git"${argv.title}" with:\n${argv.body}`)
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: () => console.log('removing note')
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: () => console.log('listing notes')
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => console.log('reading note')
+})
+
+yargs.parse()
